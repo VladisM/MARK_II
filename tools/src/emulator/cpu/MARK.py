c9b7c264315dc8ef3e8f63309c85bdf817beb7a6
Vladislav <v.mlejnecky@seznam.cz>
2017-04-26 09:54:16 +0200

    Simplify systim and remove gpio
    SysTimer is simplified by removing timer, no you have to call systim.tick()
    every time when SoC ticks.
    
    Emulatorion of GPIO is useless, so GPIO is removed.

--- tools/src/emulator/cpu/MARK.py
+++ tools/src/emulator/cpu/MARK.py
@@ -17,41 +17,37 @@ import threading
 class MARK():
 
     def __init__(self, globDef):
-        self.mycpu = cpu(self.readFunction, self.writeFunction, self.retiFunction)
-        self.myrom0 = rom(0x000000, 8, globDef, "rom0")
-        self.mygpio0 = gpio(0x000100, "gpio0")
-        self.myram0 = ram(0x000400, 10, "ram0")
-        self.myram1 = ram(0x100000, 13, "ram1")
-        self.mysystim0 = systim(0x000104, self.interrupt, globDef, "systim0")
-        self.myintControler0 = intControler(0x000108, self.mycpu, "intControler0")
+        self.cpu0 = cpu(self.readFunction, self.writeFunction, self.retiFunction)
+        self.rom0 = rom(0x000000, 8, globDef, "rom0")
+        self.ram0 = ram(0x000400, 10, "ram0")
+        self.ram1 = ram(0x100000, 13, "ram1")
+        self.systim0 = systim(0x000104, self.interrupt, "systim0")
+        self.intControler0 = intControler(0x000108, self.cpu0, "intControler0")
         self.uart0 = uart(0x00010A, self.interrupt, globDef.uart0_map, "uart0")
+
         self.F_CPU = globDef.F_CPU
         self.timObject = threading.Timer(self.F_CPU**(-1), self.tick)
 
     def readFunction(self, address):
         """CPU (master on bus) call this function to read data from specified address"""
 
-        value = self.myrom0.read(address)
+        value = self.rom0.read(address)
         if value != None:
             return value
 
-        value = self.mygpio0.read(address)
+        value = self.ram0.read(address)
         if value != None:
             return value
 
-        value = self.myram0.read(address)
+        value = self.ram1.read(address)
         if value != None:
             return value
 
-        value = self.myram1.read(address)
+        value = self.systim0.read(address)
         if value != None:
             return value
 
-        value = self.mysystim0.read(address)
-        if value != None:
-            return value
-
-        value = self.myintControler0.read(address)
+        value = self.intControler0.read(address)
         if value != None:
             return value
 
@@ -65,49 +61,45 @@ class MARK():
     def writeFunction(self, address, value):
         """Master on bus (CPU) call this function to write data into specified address"""
 
-        self.myrom0.write(address, value)
-        self.mygpio0.write(address, value)
-        self.myram0.write(address, value)
-        self.myram1.write(address, value)
-        self.mysystim0.write(address, value)
-        self.myintControler0.write(address, value)
+        self.rom0.write(address, value)
+        self.ram0.write(address, value)
+        self.ram1.write(address, value)
+        self.systim0.write(address, value)
+        self.intControler0.write(address, value)
         self.uart0.write(address, value)
 
     def hardReset(self):
         """Reset whole SoC, even peripherals that doesn't have reset input (like ROM, RAM...)"""
-        self.myrom0.reset()
-        self.mygpio0.reset()
-        self.myram0.reset()
-        self.myram1.reset()
-        self.mycpu.reset()
-        self.mysystim0.reset()
-        self.myintControler0.reset()
+        self.rom0.reset()
+        self.ram0.reset()
+        self.ram1.reset()
+        self.cpu.reset()
+        self.systim0.reset()
+        self.intControler0.reset()
         self.uart0.reset()
 
     def softReset(self):
         """Reset SoC, same as reset input on real hardware"""
-        self.mycpu.reset()
-        self.mygpio0.reset()
-        self.mysystim0.reset()
-        self.myintControler0.reset()
+        self.cpu.reset()
+        self.systim0.reset()
+        self.intControler0.reset()
         self.uart0.reset()
 
     def retiFunction(self):
-        self.myintControler0.completed()
+        self.intControler0.completed()
 
     def interrupt(self, sourceName):
-        self.myintControler0.interrupt(sourceName)
+        self.intControler0.interrupt(sourceName)
 
     def start(self):
         self.timObject.start()
-        self.mysystim0.start()
 
     def tick(self):
-        self.mycpu.tick()
+        self.cpu0.tick()
         self.uart0.tick()
+        self.systim0.tick()
         self.timObject = threading.Timer(self.F_CPU**(-1), self.tick)
         self.timObject.start()
 
     def halt(self):
         self.timObject.cancel()
-        self.mysystim0.timObject.cancel()
