fbabb04a8efaabef6dfdb7a2348adc02f4f0334e
Vladislav <v.mlejnecky@seznam.cz>
2018-05-23 23:20:35 +0200

    Typo mistake

--- toolchain/spl/include/lfsr.h
+++ toolchain/spl/include/lfsr.h
@@ -9,6 +9,6 @@
 
 #define LFSR_BASE 0x00010E /**< @brief Base of LFSR */
 
-#define LFSR_RN (*(unsigned int *)(LFSR + 0)) /**< @brief RN Register */
+#define LFSR_RN (*(unsigned int *)(LFSR_BASE + 0)) /**< @brief RN Register */
 
 #endif
